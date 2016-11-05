c3f8c2d451c507148d5dbf77a8a9352a507ef5dc
gopigithub <mobility@mobilitys-mac-mini-4.local>
2016-11-05 11:48:22 +0530

    Initial Commit

---
+++ CoreReturns/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreReturns
+//
+//  Created by Mobility on 05/11/16.
+//  Copyright © 2016 Mobility. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
